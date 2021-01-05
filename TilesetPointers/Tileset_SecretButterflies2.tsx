70bae81f6d15b3b5d0e2a89aeb37e7e2faaaaf55
sswanlake <38120033+sswanlake@users.noreply.github.com>
2021-01-04 21:01:02 -0500

    Update and rename Tileset_SecretButterflies2.tsx to TilesetPointers/Tileset_SecretButterflies2.tsx

--- TilesetPointers/Tileset_SecretButterflies2.tsx
+++ TilesetPointers/Tileset_SecretButterflies2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="SecretButterflies" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+<tileset name="ButterflyGarden" tilewidth="16" tileheight="16" tilecount="64" columns="8">
  <image source="Tilesets/tileset_SecretButterflies2.png" width="128" height="128"/>
 </tileset>
