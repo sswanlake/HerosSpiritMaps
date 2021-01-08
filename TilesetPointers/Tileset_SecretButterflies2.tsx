fb1ba8f6996e0c2ae8d2d40c710147e75c5b117f
Vera Eris Del Favero <xevra86@gmail.com>
2021-01-08 00:30:42 -0500

    Fixed links

--- TilesetPointers/Tileset_SecretButterflies2.tsx
+++ TilesetPointers/Tileset_SecretButterflies2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="ButterflyGarden" tilewidth="16" tileheight="16" tilecount="64" columns="8">
- <image source="Tilesets/tileset_SecretButterflies2.png" width="128" height="128"/>
+<tileset version="1.4" tiledversion="1.4.3" name="ButterflyGarden" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../TilesetImages/tileset_SecretButterflies2.png" width="128" height="128"/>
 </tileset>
