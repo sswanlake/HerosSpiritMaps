8ba38d1092bf69bdfb2da2af516f05170776464f
Janala Dragon <janala@dracoventions.com>
2021-07-18 09:14:20 -0700

    CastleMonillud.tmx: Add missing hearts, treasures, swords, keys, gems, illusionary walls. Add missing portal 5s and notes on how they work. Add missing NPC that gives red key hint. Move Warp layer above other layers so numbers aren't obscured. Add new icons and letters to try to make it clear what stepping in different places accomplishes.
    StartingMap:
    Use different symbols to try to clarify how to open the back door to the castle.

--- TilesetPointers/Tileset_Items.tsx
+++ TilesetPointers/Tileset_Items.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Items" tilewidth="16" tileheight="16" tilecount="81" columns="9">
- <image source="../TilesetImages/tileset_item.png" width="144" height="144"/>
+<tileset version="1.5" tiledversion="1.7.0" name="Items" tilewidth="16" tileheight="16" tilecount="117" columns="9">
+ <image source="../TilesetImages/tileset_item.png" width="144" height="208"/>
 </tileset>
