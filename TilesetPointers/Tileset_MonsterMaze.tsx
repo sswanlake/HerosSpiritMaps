5b83576d36a245a2db9ab8adbe54de47dfb433e6
sswanlake <38120033+sswanlake@users.noreply.github.com>
2021-08-12 12:17:39 -0400

    Add files via upload

---
+++ TilesetPointers/Tileset_MonsterMaze.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MonsterMaze" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../Tilesets/tileset_HauntedManse_MonsterMaze.png" width="128" height="128"/>
+</tileset>
