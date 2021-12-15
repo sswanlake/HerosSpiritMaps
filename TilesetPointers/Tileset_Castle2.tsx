874e82a8cf7bb47f5e1a6b8acc89c39d768754ea
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2021-12-15 17:23:03 +0100

    correct some of the broken paths

--- TilesetPointers/Tileset_Castle2.tsx
+++ TilesetPointers/Tileset_Castle2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Castle2" tilewidth="16" tileheight="16" tilecount="64" columns="8">
- <image source="../TilesetImages/tileset_Castle2.png" width="128" height="128"/>
+ <image source="../TilesetImages/tileset_Castle.png" width="128" height="128"/>
 </tileset>
