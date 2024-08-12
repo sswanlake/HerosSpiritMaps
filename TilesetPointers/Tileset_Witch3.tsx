c44fc78763bc60a3ef690cefa98101d178dd8558
HellaVonFreyja <154093891+HellaVonFreyja@users.noreply.github.com>
2024-08-11 18:43:16 -0700

    Add files via upload

--- TilesetPointers/Tileset_Witch3.tsx
+++ TilesetPointers/Tileset_Witch3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Witch3" tilewidth="16" tileheight="16" tilecount="64" columns="8">
- <image source="Tilesets/tileset_Witch3.png" width="128" height="128"/>
+<tileset version="1.5" tiledversion="1.7.0" name="Witch3" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../TilesetImages/tileset_Witch3.png" width="128" height="128"/>
 </tileset>
